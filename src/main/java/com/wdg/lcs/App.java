b29ae82099e859ee37c12f07b553f090ab675102
wangdg <dlwangdg@gmail.com>
2016-05-13 17:21:42 +0800

    first commit

---
+++ src/main/java/com/wdg/lcs/App.java
@@ -0,0 +1,13 @@
+package com.wdg.lcs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

72fa6f6fdedcdb0fb618ccab84d7f14c50f78a36
wangdg <dlwangdg@gmail.com>
2016-09-28 17:42:58 +0800

    UserData并发Bug修正

--- src/main/java/com/wangdg/lcs/trie/UserData.java
+++ src/main/java/com/wangdg/lcs/trie/UserData.java
@@ -2,6 +2,7 @@ package com.wangdg.lcs.trie;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * 用户数据类
@@ -10,16 +11,33 @@ public class UserData {
 
     private Map<String, Object> map = new HashMap<String, Object>();
 
+    private ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+
     public void put(String key, Object val) {
-        map.put(key, val);
+        lock.writeLock().lock();
+        try {
+            map.put(key, val);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public Object get(String key) {
-        return map.get(key);
+        lock.readLock().lock();
+        try {
+            return map.get(key);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public boolean isEmpty() {
-        return map.isEmpty();
+        lock.readLock().lock();
+        try {
+            return map.isEmpty();
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public Map<String, Object> map() {
